9862ef32c5bfef14d72c8378ee8c593f78697117
MarconyxD <71736128+MarconyxD@users.noreply.github.com>
2022-12-08 20:29:26 -0300

    Add files via upload

---
+++ Background.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private float length, startpos;
+    public float parallaxEffect;
+    public GameObject cam;
+    private Vector3 initialBackgroundPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x; 
+        length = GetComponent<SpriteRenderer>().bounds.size.x; 
+        initialBackgroundPosition = transform.position; 
+    }
+
+    void FixedUpdate()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect)); 
+        float dist = (cam.transform.position.x * parallaxEffect); 
+
+        transform.position = new Vector3(startpos + dist, initialBackgroundPosition.y, transform.position.z);
+
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos) startpos -= length;
+    }
+}
---
+++ CameraMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+    }
+}
